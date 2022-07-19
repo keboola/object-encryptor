84be5b5ae32a1a9767e05f37283e3341627fe968
odin <odinuv@gmail.com>
2022-07-19 10:40:30 +0200

    fix env file handling

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,9 +6,7 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
-    require dirname(__DIR__).'/config/bootstrap.php';
-} else {
+if (file_exists(dirname(__DIR__).'/.env')) {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env', 'dev', []);
 }
 
