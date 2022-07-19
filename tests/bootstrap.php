8ad2cc05809e15be2abbc3d6458a1a5d09422da8
odin <odinuv@gmail.com>
2022-07-19 10:14:17 +0200

    fix: import

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,11 +2,13 @@
 
 declare(strict_types=1);
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require __DIR__ . '/../vendor/autoload.php';
 
 if (file_exists(dirname(__DIR__).'/config/bootstrap.php')) {
     require dirname(__DIR__).'/config/bootstrap.php';
-} elseif (method_exists(Dotenv::class, 'bootEnv')) {
+} else {
     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env', 'dev', []);
 }
 
