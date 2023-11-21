1f0c7e52708e920451402acec3898576f7d517f4
MiroCillik <mcillik@gmail.com>
2023-11-21 14:12:47 +0100

    feat (GkmsClientFactory): set timeouts

--- src/Wrapper/GkmsClientFactory.php
+++ src/Wrapper/GkmsClientFactory.php
@@ -4,13 +4,21 @@ declare(strict_types=1);
 
 namespace Keboola\ObjectEncryptor\Wrapper;
 
+use Google\Auth\HttpHandler\HttpHandlerFactory;
 use Google\Cloud\Kms\V1\KeyManagementServiceClient;
+use GuzzleHttp\BodySummarizer;
+use GuzzleHttp\Client;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 use Keboola\ObjectEncryptor\EncryptorOptions;
 use Keboola\ObjectEncryptor\Exception\ApplicationException;
 use Throwable;
 
 class GkmsClientFactory
 {
+    public const TIMEOUT = 120;
+    public const CONNECT_TIMEOUT = 10;
+
     public function createClient(EncryptorOptions $encryptorOptions): KeyManagementServiceClient
     {
         /* It seems that KeyManagementServiceClient client does not accept retrySettings configuration
@@ -18,13 +26,39 @@ class GkmsClientFactory
             retries in GenericGKMWrapper. */
         try {
             // GKM client checks for authorization when created, authorization is cached in memory
+            $handler = HttpHandlerFactory::build($this->createHttpClient());
+
             return new KeyManagementServiceClient(
                 [
                     'disableRetries' => true,
+                    'transportConfig' => [
+                        'rest' => [
+                            'httpHandler' => [$handler, 'async'],
+                        ],
+                    ],
                 ]
             );
         } catch (Throwable $e) {
             throw new ApplicationException('Cipher key settings are invalid: ' . $e->getMessage(), 0, $e);
         }
+    }
+
+    private function createHttpClient(): Client
+    {
+        // copied from HttpHandlerFactory::build()
+        $stack = null;
+        if (class_exists(BodySummarizer::class)) {
+            // double the # of characters before truncation by default
+            $bodySummarizer = new BodySummarizer(240);
+            $stack = HandlerStack::create();
+            $stack->remove('http_errors');
+            $stack->unshift(Middleware::httpErrors($bodySummarizer), 'http_errors');
+        }
+
+        return new Client([
+            'timeout' => self::TIMEOUT,
+            'connect_timeout' => self::CONNECT_TIMEOUT,
+            'handler' => $stack,
+        ]);
     }
 }
